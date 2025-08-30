131a12fb84b71430bb62fc73b5b49b5762518bfe
Eric Traut <eric@traut.com>
2025-08-30 15:39:43 -0700

    Fixed regression introduced in 1.1.343 that results in nondeterministic (order-dependent) type evaluation under certain circumstances involving recursive type aliases. This addresses #10850. (#10865)

--- packages/pyright-internal/src/analyzer/typeEvaluator.ts
+++ packages/pyright-internal/src/analyzer/typeEvaluator.ts
@@ -23452,7 +23452,9 @@ export function createTypeEvaluator(
                     const usageScope = ParseTreeUtils.getExecutionScopeNode(usageNode);
                     const declScope = ParseTreeUtils.getExecutionScopeNode(decl.node);
                     if (usageScope === declScope) {
-                        return;
+                        if (!isFlowPathBetweenNodes(decl.node, usageNode)) {
+                            return;
+                        }
                     }
                 }
             }
